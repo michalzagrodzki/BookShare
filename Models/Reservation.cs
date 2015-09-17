575276454bed7a7ec4ce3211f726979df0c3242f
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-17 14:24:59 +0200

    Add and define Reservation model.

---
+++ Models/Reservation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookShare.Models
+{
+    public class Reservation
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime ReservationDate { get; set; }
+    }
+}
