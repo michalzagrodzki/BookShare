479bc70b370afc1a34ede16ca6e40976e584d5e2
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-17 13:52:31 +0200

    Define Book model.

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,5 +7,13 @@ namespace BookShare.Models
 {
     public class Book
     {
+        public int ID { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CoverImage { get; set; }
+        public string Publisher { get; set; }
+        public int PublishedDate { get; set; }
+        public int PageCount { get; set; }
     }
 }
