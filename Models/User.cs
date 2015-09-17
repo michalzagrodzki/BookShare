9a05beaa33deecd11989637e780fb3c9d6793774
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-17 21:58:00 +0200

    Modify User model.

--- Models/User.cs
+++ Models/User.cs
@@ -8,12 +8,13 @@ namespace BookShare.Models
 {
     public class User : IdentityUser
     {
+    
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Bio { get; set; }
         public string ProfilePicture { get; set; }
-
+        
         public virtual ICollection<Book> Books { get; set; }
         public virtual Location Location { get; set; }
     }
