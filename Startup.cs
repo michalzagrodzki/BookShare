d4eeb268034bb80f01517ceb17f0f2c4a344d0b6
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-17 13:08:44 +0200

    Init repository. Add gitinore file.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookShare.Startup))]
+namespace BookShare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
