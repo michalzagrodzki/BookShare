795c45b49e842161fd39cc78986ce7d41bf93960
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-17 15:34:35 +0200

    Add and define User model.

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookShare.Models
+{
+    public class User : IdentityUser
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+        public string ProfilePicture { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+        public virtual Location Location { get; set; }
+    }
+}
