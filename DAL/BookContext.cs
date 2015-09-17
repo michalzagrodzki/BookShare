361fe1249d8fbc16420247b134e63a0305b4d98c
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-17 18:45:05 +0200

    Create database schema.

---
+++ DAL/BookContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using BookShare.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace BookShare.DAL
+{
+    public class BookContext : DbContext
+    {
+        public BookContext() : base("BookContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
